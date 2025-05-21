afa5b1ef23ab2ed6cd0f98372ba4766cbf890138
TrueKnowledge <goust277@gmail.com>
2025-05-21 23:51:36 +0900

    수정

--- Assets/Script/ScnenChange/NextScene.cs
+++ Assets/Script/ScnenChange/NextScene.cs
@@ -7,18 +7,6 @@ using UnityEngine.SceneManagement;
 public class NextScene : MonoBehaviour
 {
     [SerializeField] private string SceneName;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
