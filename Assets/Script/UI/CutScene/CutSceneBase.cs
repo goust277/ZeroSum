3f14e5c2b6bd6210bc636ec7987b26e21fafeb07
ksr-ccb <harang4282@gmail.com>
2025-05-22 20:49:20 +0900

    [추가] 스테이지2 시네마틱1

--- Assets/Script/UI/CutScene/CutSceneBase.cs
+++ Assets/Script/UI/CutScene/CutSceneBase.cs
@@ -2,27 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Com.LuisPedroFonseca.ProCamera2D;
+using TMPro.Examples;
 
 public class CutSceneBase : MonoBehaviour
 {
     [SerializeField] protected ProCamera2D proCamera2D;
     [SerializeField] protected Transform[] cutsceneTarget; // 컷씬 시 볼 지점
-     protected Transform playerTarget;
+    [SerializeField] protected GameObject[] dialogs; // 컷씬 시 볼 지점
+
+    protected GameObject player;
+    protected Transform playerTarget;
+    protected GameObject inputManager;
+    [SerializeField] protected Animator playerAnimator;
+
     [SerializeField] protected float zoomDuringCutscene = 4.5f;
     [SerializeField] protected float zoomSpeed = 1.5f;
 
     protected float originOrthographic = 6.7f;
     protected bool hasPlayed = false;
+    private Coroutine moveCoroutine;
+
+
+
+    [Header("BackGround")]
+    [SerializeField] protected GameObject up;
+    [SerializeField] protected GameObject down;
 
     protected void Start()
     {
-        playerTarget = GameObject.Find("Player").transform;
+        player = GameObject.Find("Player");
+        playerTarget = player.transform;
+        inputManager = GameObject.Find("InputManager");
+
+        if(inputManager == null || player == null)
+        {
+            Debug.LogError("못찾음");
+        }
+
     }
 
     protected void StartCutScene()
     {
         // 플레이어 따라다니는 거 제거
         proCamera2D.RemoveAllCameraTargets();
+
+        up.SetActive(true);
+        down.SetActive(true);
+        StartCoroutine(MoveUIVerticallyDown(up, 100.0f));
+        StartCoroutine(MoveUIVerticallyUp(down, 100.0f));
+
+        if (inputManager != null)
+            inputManager.SetActive(false);
     }
 
     protected void EndCutScene()
@@ -30,6 +60,137 @@ public class CutSceneBase : MonoBehaviour
         float startZoom = Camera.main.orthographicSize;
 
         proCamera2D.AddCameraTarget(playerTarget, 0f, 2f);
-        proCamera2D.Zoom(startZoom - originOrthographic, 2.0f);
+        proCamera2D.Zoom(originOrthographic - startZoom, 2.0f);
+
+        player.GetComponent<PlayerAnimation>().enabled = true;
+        up.SetActive(false);
+        down.SetActive(false);
+        StartCoroutine(MoveUIVerticallyDown(down, 100.0f));
+        StartCoroutine(MoveUIVerticallyUp(up, 100.0f));
+
+        if (inputManager != null)
+            inputManager.SetActive(true);
     }
+
+    public void MoveAndZoomTo(Vector2 targetPosition, float targetZoom, float duration)
+    {
+        StartCoroutine(MoveCameraCoroutine(targetPosition, targetZoom, duration));
+    }
+
+    private IEnumerator MoveCameraCoroutine(Vector2 targetPosition, float targetZoom, float duration)
+    {
+        Vector3 startPos = proCamera2D.transform.position;
+        Vector3 endPos = new Vector3(targetPosition.x, targetPosition.y, startPos.z);
+
+        float startZoom = Camera.main.orthographicSize;
+        float elapsed = 0f;
+
+        proCamera2D.Zoom(targetZoom - startZoom, duration);
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / duration);
+
+            proCamera2D.transform.position = Vector3.Lerp(startPos, endPos, t);
+
+            yield return null;
+        }
+
+        proCamera2D.transform.position = endPos;
+    }
+
+    private IEnumerator MoveUIVerticallyDown(GameObject targetObj, float distance)
+    {
+        RectTransform target = targetObj.transform as RectTransform;
+
+        Vector2 startPos = target.anchoredPosition;
+        Vector2 endPos = startPos + new Vector2(0f, distance *-1f);
+        float duration = 1f;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.unscaledDeltaTime;  // TimeScale 영향 없이 실행
+            float t = Mathf.Clamp01(elapsed / duration);
+            target.anchoredPosition = Vector2.Lerp(startPos, endPos, t);
+            yield return null;
+        }
+
+        target.anchoredPosition = endPos;
+    }
+
+    private IEnumerator MoveUIVerticallyUp(GameObject targetObj, float distance)
+    {
+        RectTransform target = targetObj.transform as RectTransform;
+
+        Vector2 startPos = target.anchoredPosition;
+        Vector2 endPos = startPos + new Vector2(0f, distance);
+        float duration = 1f;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.unscaledDeltaTime;  // TimeScale 영향 없이 실행
+            float t = Mathf.Clamp01(elapsed / duration);
+            target.anchoredPosition = Vector2.Lerp(startPos, endPos, t);
+            yield return null;
+        }
+
+        target.anchoredPosition = endPos;
+    }
+
+    public void StartPlayerMove(Transform targetTransform, float duration)
+    {
+        if (moveCoroutine != null)
+            StopCoroutine(moveCoroutine);
+
+        moveCoroutine = StartCoroutine(MovePlayerTo(targetTransform, duration));
+    }
+
+    protected IEnumerator MovePlayerTo(Transform targetPoint, float duration)
+    {
+        float moveSpeed = 2f;
+        float startTime = Time.time;
+
+        // y, z 고정한 목표 위치
+        Vector3 targetPos = new Vector3(targetPoint.position.x, playerTarget.position.y, playerTarget.position.z);
+
+        // 현재 오른쪽 바라보는 상태인가? → y=0이면 true
+        bool isFacingRight = Mathf.Approximately(playerTarget.eulerAngles.y, 0f);
+
+        // 목표 위치에 따라 회전 조절
+        if (targetPoint.position.x < playerTarget.position.x && isFacingRight)
+        {
+            // 왼쪽으로 회전
+            playerTarget.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (targetPoint.position.x > playerTarget.position.x && !isFacingRight)
+        {
+            // 오른쪽으로 회전
+            playerTarget.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        // 애니메이션 시작
+        player.GetComponent<PlayerAnimation>().enabled = false;
+        playerAnimator.SetBool("Move", true);
+
+        while (Mathf.Abs(playerTarget.position.x - targetPos.x) > 0.05f && Time.time - startTime < duration)
+        {
+            Vector3 dir = (targetPos - playerTarget.position).normalized;
+            playerTarget.position += dir * moveSpeed * Time.deltaTime;
+
+            yield return null;
+        }
+
+        // 위치 보정
+        playerTarget.position = targetPos;
+        // 애니메이션 정지
+        playerAnimator.SetBool("Move", false);
+        playerAnimator.Play("Idle");
+
+        moveCoroutine = null;
+    }
+
+
 }
--- Assets/Script/UI/CutScene/Stage2/Stage2_Num1Scene.cs
+++ Assets/Script/UI/CutScene/Stage2/Stage2_Num1Scene.cs
@@ -1,16 +1,217 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Stage2_Num1Scene : CutSceneBase
 {
-    [SerializeField] private Collider2D[] evs;
+    [SerializeField] private Collider2D trigger;
+    [SerializeField] private MovingBlock[] evs;
+    [SerializeField] private Transform move;
+    [SerializeField] private Vector3 npcmove;
+
+    [SerializeField] private GameObject fade;
+    [SerializeField] private GameObject npc;
+    [SerializeField] private GameObject scout;
+    [SerializeField] private AudioSource battleAudio;
+    [SerializeField] private AudioSource runAudio;
+
+    [Header("Error Ani")]
+    [SerializeField] private GameObject error9;
+    [SerializeField] private Image error9img;
+    [SerializeField] private TextMeshProUGUI error9txt;
+
+    private void Start()
+    {
+        base.Start();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!hasPlayed && collision.CompareTag("Player"))
         {
-            Debug.Log("BeforeEvUp - OnTriggerEnter2D");
+            trigger.enabled= false;
+            evs[0].enabled = false;
+            evs[1].enabled = false;
+            StartCutScene();
+            StartCoroutine(From1To3());
+        }
+    }
+
+    private IEnumerator ShowDialog(int index, float duration)
+    {
+        dialogs[index].SetActive(true);
+        yield return new WaitForSeconds(duration);
+        dialogs[index].SetActive(false);
+    }
+
+    private IEnumerator From1To3(){
+        MoveAndZoomTo((Vector2)cutsceneTarget[0].position, 4.0f, 2.0f);
+        yield return MovePlayerTo(move, 2.0f);
+
+        yield return ShowDialog(0, 6.5f);
+
+        MoveAndZoomTo((Vector2)cutsceneTarget[1].position, 4.0f, 2.0f);
+        yield return new WaitForSeconds(3.0f);
+        playerTarget.rotation = Quaternion.Euler(0, 0, 0);
+
+        dialogs[1].SetActive(true);
+        yield return new WaitForSeconds(2.0f);
+        //6
+        dialogs[2].SetActive(true);
+        yield return new WaitForSeconds(2.0f);
+        dialogs[1].SetActive(false);
+        dialogs[2].SetActive(false);
+
+        for (int i = 0; i < 5; i++)
+        {
+            runAudio.PlayOneShot(runAudio.clip);
+            yield return new WaitForSeconds(0.3f);
+        }
+
+        SpriteRenderer sr = npc.GetComponent<SpriteRenderer>();
+        sr.sortingOrder = 4;
+        npc.transform.position = npcmove;
+        yield return new WaitForSeconds(1.0f);
+
+        //NPC IDLE 뒤집기
+        dialogs[3].SetActive(true);
+        //7~8
+        npc.transform.rotation = Quaternion.Euler(0, 0, 0); //우
+        yield return new WaitForSeconds(2.0f);
+        npc.transform.rotation = Quaternion.Euler(0, 180, 0); //좌
+        yield return new WaitForSeconds(3.0f);
+        dialogs[3].SetActive(false);
+        dialogs[4].SetActive(true);
+        //두리번두리번
+        npc.transform.rotation = Quaternion.Euler(0, 0, 0); //우
+        yield return new WaitForSeconds(1.0f);
+        npc.transform.rotation = Quaternion.Euler(0, 180, 0); //좌
+        yield return new WaitForSeconds(1.0f);
+        dialogs[4].SetActive(false);
+        npc.transform.rotation = Quaternion.Euler(0, 0, 0);
+        // 9
+        yield return new WaitForSeconds(1.0f);
+        npc.transform.rotation = Quaternion.Euler(0, 180, 0);
+        StartCoroutine(ErrorBlink());
+        // 10
+        yield return ShowDialog(5, 3.0f);
+
+        // 11
+        dialogs[6].SetActive(true);
+        yield return new WaitForSeconds(4.0f);
+        dialogs[7].SetActive(true); //12
+        yield return new WaitForSeconds(2.0f);
+        dialogs[7].SetActive(false);
+        dialogs[6].SetActive(false);
+
+        //13
+        yield return ShowDialog(8, 2.0f);
+
+        //14
+        yield return ShowDialog(9, 4.0f);
+
+        //15
+        yield return ShowDialog(10, 2.0f);
+
+        //16
+        dialogs[11].SetActive(true);
+        yield return new WaitForSeconds(3.0f);
+        dialogs[12].SetActive(true);
+        yield return new WaitForSeconds(4.0f);
+        dialogs[11].SetActive(false);
+        dialogs[12].SetActive(false);
+
+        yield return ShowDialog(13, 2.0f);
+
+        //정찰자  걸어와야함
+        MoveAndZoomTo((Vector2)cutsceneTarget[2].position, 5.12f, 2.0f);
+        yield return new WaitForSeconds(1.8f);
+        scout.SetActive(true);
+
+        dialogs[14].SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        dialogs[15].SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        npc.transform.rotation = Quaternion.Euler(0, 0, 0); //우
+        yield return new WaitForSeconds(1.0f);
+        dialogs[14].SetActive(false);
+        dialogs[15].SetActive(false);
+
+        //쏘기 모션
+        yield return new WaitForSeconds(1.0f);
+        fade.SetActive(true);
+        yield return new WaitForSeconds(1.0f);
+        battleAudio.PlayOneShot(battleAudio.clip);
+        yield return new WaitForSeconds(0.4f);
+        battleAudio.PlayOneShot(battleAudio.clip);
+        yield return new WaitForSeconds(0.5f);
+        scout.GetComponent<Scout>().Damage(2);
+        scout.SetActive(false);
+        battleAudio.PlayOneShot(battleAudio.clip);
+        MoveAndZoomTo((Vector2)cutsceneTarget[3].position, 4.0f, 1.0f);
+        yield return new WaitForSeconds(1.0f);
+        fade.SetActive(false);
+
+        //숙인거 다시 일어서기
+        yield return new WaitForSeconds(1.0f);
+
+        //21
+        yield return ShowDialog(16, 3.0f);
+        yield return new WaitForSeconds(1.0f);
+        yield return ShowDialog(17, 3.0f);
+
+        npc.GetComponent<NPCController>().enabled = true;
+        yield return new WaitForSeconds(1.0f);
+        MoveAndZoomTo((Vector2)cutsceneTarget[4].position, 7.9f, 2.0f);
+        yield return new WaitForSeconds(2.0f);
+        MoveAndZoomTo((Vector2)cutsceneTarget[5].position, 7.9f, 5.0f);
+        yield return new WaitForSeconds(5.0f);
+        EndCutScene();
+    }
+
+    IEnumerator ErrorBlink()
+    {
+        Color32 fromColor = new Color32(255, 0, 0, 255);
+        Color32 toColor = new Color32(118, 0, 0, 255);
+        float duration = 1f;
+
+        error9.SetActive(true);
+        float endAlpha = 70f / 255f;
+        Color originalColor = error9img.color;
+        float elapsed = 0f;
+        
+        while (elapsed < 1f)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / 1f);
+
+            float newAlpha = Mathf.Lerp( 0f, endAlpha, t);
+            error9img.color = new Color(originalColor.r, originalColor.g, originalColor.b, newAlpha);
+
+            yield return null;
+        }
+
+        while (true)
+        {
+            // 1. from → to
+            float t = 0f;
+            while (t < 1f)
+            {
+                t += Time.deltaTime / duration;
+                error9txt.color = Color32.Lerp(fromColor, toColor, t);
+                yield return null;
+            }
+
+            // 2. to → from
+            t = 0f;
+            while (t < 1f)
+            {
+                t += Time.deltaTime / duration;
+                error9txt.color = Color32.Lerp(toColor, fromColor, t);
+                yield return null;
+            }
         }
     }
 }
