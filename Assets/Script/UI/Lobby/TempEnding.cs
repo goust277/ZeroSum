dc58f1d56364d88c2fcdd25ab83915d373a67a6e
Ksr-ccb <harang4282@gmail.com>
2025-05-29 06:33:42 +0900

    [추가] 마지막씬 수정

---
+++ Assets/Script/UI/Lobby/TempEnding.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempEnding : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameStateManager.Instance.StartMoveUIUp();  
+    }
+}
